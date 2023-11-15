990a3403d00c7222fdc63026973fd3b39c14ee75
sriakkineni2002 <sriakkineni2002@vt.edu>
2023-11-15 14:29:46 -0500

    added code for hello

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+        String myName = "Sriniketh Akkineni"; 
+        System.out.println("Hello, my name is " + myName);
+    }
+}
+
